795556834a2d5406f2a7c35b0d3361d4dea748e9
alfu-git <mdalfaz.dev@gmail.com>
2026-04-15 17:48:03 +0600

    create scroll auto reset function when the page is changed

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/navbar/Navbar";
 import Footer from "../components/shared/footer/Footer";
+import ScrollToTop from "../components/shared/scrollToTop/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
+
       <header>
         <Navbar />
       </header>
